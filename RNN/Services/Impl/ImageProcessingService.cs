07ea391de0cfdd4cd7be8a31b0fc7328236cc2ea
Demian Floreani <demian.floreani@complevo.de>
2020-05-21 12:48:10 +0200

    rollback

--- RNN/Services/Impl/ImageProcessingService.cs
+++ RNN/Services/Impl/ImageProcessingService.cs
@@ -1,6 +1,7 @@
 ﻿using ImageMagick;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Migrations;
 using RNN.Services;
 using System;
 using System.Collections.Generic;
@@ -21,28 +22,79 @@ namespace RNN.Services.Impl
 
         public string ProcessFormImage(IFormFile file)
         {
-            string imageName = String.Concat(Guid.NewGuid().ToString(), ".jpg");
-            var fileName = Path.Combine(_environment.WebRootPath, "images", "uploads", imageName);
+            string identifier = String.Concat(Guid.NewGuid().ToString());
+            string uploadPath = Path.Combine(_environment.WebRootPath, "images", "uploads");
 
-            SaveFormFileAsJpg(file, fileName);
-            Compress(fileName);
+            //var fileName = Path.Combine(_environment.WebRootPath, "images", "uploads", imageName);
 
-            return imageName;
+            //SaveFormFileAsJpg(file, fileName);
+            //Compress(fileName);
+
+            ResizeImageToMultipleViewSizes(file, uploadPath, identifier);
+
+            return identifier;
         }
 
         private void SaveFormFileAsJpg(IFormFile file, string output)
         {
-            using (var stream = new FileStream(output, FileMode.Create))
+            using (MagickImage image = new MagickImage(file.OpenReadStream()))
             {
-                file.CopyTo(stream);
+                try
+                {
+                    image.Resize(new MagickGeometry()
+                    {
+                        Width = 600,
+                        IgnoreAspectRatio = false
+                    });
+                    image.Format = MagickFormat.WebP;
+                    image.Write(output);
+                }
+                catch (Exception ex)
+                {
+                    var ms = ex.Message;
+                }
             }
-            //File.WriteAllBytes(output, file.);
 
-            //using (MagickImage image = new MagickImage(file.OpenReadStream()))
-            //{
-            //    image.Format = MagickFormat.Jpg;
-            //    image.Write(output);
-            //}
+            //ResizeImageToMultipleViewSizes(output);
+        }
+
+        private void ResizeImageToMultipleViewSizes(IFormFile file, string uploadPath, string identifier)
+        {
+            using (MagickImage image = new MagickImage(file.OpenReadStream()))
+            {
+                // create image for small devices
+                ResizeTo(image, 375, "tiny", uploadPath, identifier);
+            }
+
+            using (MagickImage image = new MagickImage(file.OpenReadStream()))
+            {
+                if (image.Width > 510)
+                {
+                    // create image for medium devices
+                    ResizeTo(image, 510, "medium", uploadPath, identifier);
+                }
+            }
+
+            using (MagickImage image = new MagickImage(file.OpenReadStream()))
+            {
+                if (image.Width > 730)
+                {
+                    // create image for large devices
+                    ResizeTo(image, 730, "large", uploadPath, identifier);
+                }
+            }
+
+        }
+
+        private void ResizeTo(MagickImage image, int width, string device, string uploadPath, string identifier)
+        {
+            image.Resize(new MagickGeometry()
+            {
+                Width = width,
+                IgnoreAspectRatio = false
+            });
+
+            image.Write(Path.Combine(uploadPath, String.Format("{0}-{1}.WebP", identifier, device)));
         }
 
         private static void Compress(string file)
